fac0bac697ad55e072cf790a6a7cf6f01e026f77
Maher Attouche <maherattouche7@gmail.com>
2020-05-18 01:59:32 +0200

    Interpreter

--- src/main/java/uvsq/fr/pglp_9_9/DrawingTui.java
+++ src/main/java/uvsq/fr/pglp_9_9/DrawingTui.java
@@ -12,9 +12,50 @@ public class DrawingTui {
                 new BufferedReader(new InputStreamReader(System.in));
      String cmd = reader.readLine();
      
-     cmd.replace(" ", "");
+     cmd.replaceAll(",", " ");
+     cmd.replaceAll("=", " ");
+     cmd.replaceAll("(", " ");
+     cmd.replaceAll(")", " ");
+     
+     String[] tabCmd=cmd.split(" ");
+     
+     if(tabCmd[0]=="move")
+     {
+    	 
+     }
+     if(tabCmd[0]=="print")
+     {
+    	 
+     }
+     if(tabCmd[0]!="print" && tabCmd[0]!="move")
+     {
+    	 if(tabCmd[1]=="cercle")
+    	 {
+    		 DrawableShape sp=new Cercle(tabCmd[0],new Point(Integer.parseInt(tabCmd[2]),Integer.parseInt(tabCmd[3])),Integer.parseInt(tabCmd[4]));
+    		 return new CreateCommand(sp);
+    	 }
+    	 if(tabCmd[1]=="carre")
+    	 {
+    		 DrawableShape sp=new Carre(tabCmd[0],new Point(Integer.parseInt(tabCmd[2]),Integer.parseInt(tabCmd[3])),Integer.parseInt(tabCmd[4]));
+    		 return new CreateCommand(sp);
+    	 }
+    	 if(tabCmd[1]=="rectangle")
+    	 {
+    		 DrawableShape sp=new Rectangle(tabCmd[0],new Point(Integer.parseInt(tabCmd[2]),Integer.parseInt(tabCmd[3])),Integer.parseInt(tabCmd[4]),Integer.parseInt(tabCmd[5]));
+    		 return new CreateCommand(sp);
+    	 }
+    	 if(tabCmd[1]=="triangle")
+    	 {
+    		 DrawableShape sp=new Triangle(tabCmd[0],new Point(Integer.parseInt(tabCmd[2]),Integer.parseInt(tabCmd[3])),Integer.parseInt(tabCmd[4]),Integer.parseInt(tabCmd[5]),Integer.parseInt(tabCmd[6]));
+    		 return new CreateCommand(sp);
+    	 }
+    	 if(tabCmd[1]=="composite")
+    	 {
+    		 DrawableShape sp=new CompositeShape(tabCmd[0],new Point(Integer.parseInt(tabCmd[2]),Integer.parseInt(tabCmd[3])));
+    		 return new CreateCommand(sp);
+    	 }
+     }
       
-     // create a matcher that will match the given input against this pattern
      
 		return null;
 		
